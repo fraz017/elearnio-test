962a49d7d4354ed8e470c5c0ce5e333f64d81934
Fraz Ahsan <fraz.ahsan17@gmail.com>
2022-05-28 20:20:20 +0500

    Test fixtures added

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -4,4 +4,15 @@ class AuthorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'Valid author' do
+    author = Author.new(name: 'John', email: 'john@example.com')
+    assert author.valid?
+  end
+
+  test 'Invalid author without name' do
+    author = Author.new(email: 'john@example.com')
+    refute author.valid?, 'user is valid without a name'
+    assert_not_nil author.errors[:name], 'no validation error for name present'
+  end
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,15 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'Valid Course' do
+    course = Course.new(name: 'Test Course', author_id: 1)
+    assert course.valid?
+  end
+
+  test 'Invalid course without author' do
+    course = Course.new(name: 'Test Course')
+    refute course.valid?, 'course is not valid without an author'
+    assert_not_nil course.errors[:author_id], 'no validation error for name present'
+  end
 end
